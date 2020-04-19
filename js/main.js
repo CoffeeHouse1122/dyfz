264e07694f3ca109740d949614639aade16b2fc3
CoffeeHouse1122 <1158165115@qq.com>
2020-04-19 22:56:51 +0800

    前端静态页完成

--- js/main.js
+++ js/main.js
@@ -31,12 +31,43 @@ const $axios = axios.create({
     }
   )
   /* ======================= axios end ======================= **/
-
+// 顶部导航
 $(".header-menuBtn").click(function() {
   if($(".header-nav").hasClass("cur")) {
     $(".header-nav").removeClass("cur")
   } else {
     $(".header-nav").addClass("cur")
-
   }
+})
+// 左侧分类切换
+$(".com-left-con li").click(function() {
+  var i = $(this).index()
+  $(".com-left-con li").eq(i).addClass("cur").siblings().removeClass("cur")
+  $(".com-sideBar-con li").eq(i).addClass("cur").siblings().removeClass("cur")
+})
+$(".com-sideBar-con li").click(function() {
+  var i = $(this).index()
+  $(".com-left-con li").eq(i).addClass("cur").siblings().removeClass("cur")
+  $(".com-sideBar-con li").eq(i).addClass("cur").siblings().removeClass("cur")
+})
+
+$(".com-sideBar-btn").click(function() {
+  if($(".com-sideBar-mask").hasClass("cur")) {
+    $(".com-sideBar-mask").removeClass("cur")
+    $(".com-sideBar-con").removeClass("cur")
+  } else {
+    $(".com-sideBar-mask").addClass("cur")
+    $(".com-sideBar-con").addClass("cur")
+  }
+})
+
+$(".com-sideBar-mask").click(function() {
+  $(".com-sideBar-mask").removeClass("cur")
+  $(".com-sideBar-con").removeClass("cur")
+})
+
+// 产品二级分类切换
+$(".pl-right-type li").click(function() {
+  var i = $(this).index()
+  $(".pl-right-type li").eq(i).addClass("cur").siblings().removeClass("cur")
 })
